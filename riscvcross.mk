14efcd03ec0cc06e886862c4ea95527c66969d1d
THomas Hornschuh <thomas@bonfire.eu>
2019-02-24 23:46:22 +0100

    Makefile update

--- riscvcross.mk
+++ riscvcross.mk
@@ -3,7 +3,8 @@
 build:
 	make CROSS_COMPILE=riscv64-unknown-elf-  ARCH=rv32im  PREFIX=build_rv32 \
 	 IPFILTER=0 DHCP_SERVER=0 NAT=0 MCAST=0 MDNS=0 \
-	 PPP=0 IPV6=0 AODV=0 MDNS=0
+	 PPP=0 IPV6=0 AODV=0 MDNS=0 \
+	 PLATFORM_CFLAGS=" -Os -ffunction-sections -fdata-sections"
 	
 clean:
 	make clean  PREFIX=build_rv32
